9e765a71def5f713be4ae23ad302bdca100ab729
Theo02-12 <mrcouvelard@gmail.com>
2023-04-03 16:43:58 +0200

    POST

--- app.js
+++ app.js
@@ -1,13 +1,73 @@
 let quote = document.querySelector("#quote");
 let author = document.querySelector("#author");
 
-setInterval(getRandomCitation, 5000)
 
+const bntSubmit = document.querySelector('#submit');
+const authorValue = document.querySelector('#authorValue');
+const quoteValue = document.querySelector('#citation');
+
+
+
+function post() {
+    console.log(authorValue.value, quoteValue.value);
+
+    valueAuthor = authorValue.value;
+    valueQuote = quoteValue.value;
+
+
+
+    const value2 = {
+        "data": {
+            "auteur": valueAuthor,
+        }
+    };
+var newId;
+
+fetch("http://localhost:1337/api/auteurs", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(value2)
+    }).then(response => response.json())
+        .then(data => {
+            console.log(data); 
+            newId = data.data.id;
+
+            console.log(newId);
+        })
+        
+        const value3 = {
+        "data": {
+            "citation": valueQuote,
+            "auteur": {
+                "id": newId
+                }
+            }
+        }
+
+fetch("http://localhost:1337/api/citations/?populate=*", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(value3)
+    }).then(response => response.json())
+        .then(data => {
+            console.log(data);
+           
+        })
+    };
+
+bntSubmit.addEventListener('click', post);
+
+
+
+//setInterval(getRandomCitation, 5000)
 function getRandomCitation() {
     fetch('http://localhost:1337/api/citations/?populate=*')
         .then(res => res.json())
         .then(data => {
-
 
             const randomQuote = data.meta.pagination.total - 1;
 
